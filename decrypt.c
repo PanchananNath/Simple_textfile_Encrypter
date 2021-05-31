f147120959aac7691e91f8406646a9950a407b13
EncryptedHacker <77277835+PanchananNath@users.noreply.github.com>
2021-05-31 12:21:28 +0530

    Add files via upload

---
+++ decrypt.c
@@ -0,0 +1,28 @@
+/*Program to Decrypt the encrypted  File*/
+
+
+#include<stdio.h>
+int main()
+{
+    char fname[100], ch;
+    FILE *fps, *fpt;                // *fps and *fpt are two file pointer.
+    printf("Enter Filename: ");
+    gets(fname);                    // gets(); function helps to get the filename entered by the user.
+    fps = fopen(fname, "w");        // open the file provided by the use in write mode.
+    if(fps == NULL)                 // if file name is missing then the program will be terminated.
+        return 0;
+    fpt = fopen("cipher.txt", "r");
+    if(fpt == NULL)
+        return 0;
+    ch = fgetc(fpt);                // fgetc(); helps to read each character present in the given file.
+    while(ch != EOF)                // loop will continue till the End of File (EOF).
+    {
+        ch = ch-8;                  // change the ASCCI value of each character by subtracting 8 from it.
+        fputc(ch, fps);             //update the charter value in the *fps file pointer with the help of swapping.
+        ch = fgetc(fpt);
+    }
+    fclose(fps);
+    fclose(fpt);
+    printf("\nFile %s Decrypted Successfully!", fname);
+    return 0;
+}
---
+++ encrypt.c
@@ -0,0 +1,41 @@
+/*Program to Encrypt a text File*/
+
+#include<stdio.h>
+int main()
+{
+    char fname[100], ch;
+    FILE *fps, *fpt;                // *fps and *fpt are two file pointer.
+    printf("Enter Filename: ");
+    gets(fname);                    // gets(); function helps to get the filename entered by the user.
+    fps = fopen(fname, "r");        // open the file provided by the use in read mode.
+    if(fps == NULL)                 // if file name is missing then the program will be terminated.
+        return 0;
+    fpt = fopen("cipher.txt", "w");
+    if(fpt == NULL)
+        return 0;
+    ch = fgetc(fps);
+    while(ch != EOF)
+    {
+        ch = ch+8;
+        fputc(ch, fpt);
+        ch = fgetc(fps);
+    }
+    fclose(fps);
+    fclose(fpt);
+    fps = fopen(fname, "w");
+    if(fps == NULL)
+        return 0;
+    fpt = fopen("cipher.txt", "r");
+    if(fpt == NULL)
+        return 0;
+    ch = fgetc(fpt);
+    while(ch != EOF)
+    {
+        ch = fputc(ch, fps);
+        ch = fgetc(fpt);
+    }
+    fclose(fps);
+    fclose(fpt);
+    printf("\nFile %s Encrypted Successfully!", fname);
+        return 0;
+}
